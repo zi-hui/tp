e8389f616c0f8f3f8d9d34477f4bfc459494c013
jinfayap <jinfayap@gmail.com>
2020-03-06 19:23:40 +0800

    Update InvalidCommand functionality

--- src/main/java/seedu/kitchenhelper/command/InvalidCommand.java
+++ src/main/java/seedu/kitchenhelper/command/InvalidCommand.java
@@ -6,12 +6,41 @@ import seedu.kitchenhelper.object.ingredient.Ingredient;
 
 import java.util.ArrayList;
 
+/**
+ * Represent an invalid command. Upon execution, produces some feedback to the user.
+ */
 public class InvalidCommand extends Command {
     
-    private static final String MESSAGE_INVALID = "Invalid Command";
-
+    public static final String MESSAGE_INVALID = "Invalid Command Format!";
+    
+    private final String invalidFeedback;
+    
+    /**
+     * Constructor for InvalidCommand.
+     */
+    public InvalidCommand() {
+        this.invalidFeedback = MESSAGE_INVALID;
+    }
+    
+    /**
+     * Constructor for InvalidCommand.
+     *
+     * @param invalidFeedback feedback for invalid command message.
+     */
+    public InvalidCommand(String invalidFeedback) {
+        this.invalidFeedback = invalidFeedback;
+    }
+    
+    /**
+     * {@inheritDoc}
+     *
+     * @param ingredientList list of ingredients.
+     * @param recipeList     list of recipes.
+     * @param choreList      list of chores.
+     * @return the invalid message.
+     */
     public CommandResult execute(ArrayList<Ingredient> ingredientList, ArrayList<Recipe> recipeList,
                                  ArrayList<Chore> choreList) {
-        return new CommandResult(MESSAGE_INVALID);
+        return new CommandResult(invalidFeedback);
     }
 }
