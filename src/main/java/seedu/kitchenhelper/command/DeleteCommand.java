3847bfe8818976a569418d309630ef93d82e5130
yantingsanity <lytemail18@gmail.com>
2020-03-08 00:26:42 +0800

    fixed delete bug

--- src/main/java/seedu/kitchenhelper/command/DeleteCommand.java
+++ src/main/java/seedu/kitchenhelper/command/DeleteCommand.java
@@ -104,15 +104,18 @@ public class DeleteCommand extends Command {
     public String deleteRecipe(String recipeName, ArrayList<Recipe> recipeList) {
         String feedbackToUser;
         int count = 0;
+        int index = 0;
+
+        index = recipeList.indexOf(recipeName);
 
         for (Recipe recipe : recipeList) {
-            System.out.println(recipe.getRecipeName());
             if (recipe.getRecipeName().equalsIgnoreCase(recipeName.trim())) {
-                recipeList.remove(recipe);
-                count += 1;
+                break;
             }
+            count += 1;
         }
-        if (count == 1) {
+        if (count <= (recipeList.size() -1) && (count >= 0)) {
+            recipeList.remove(recipeList.get(count));
             feedbackToUser = recipeName + " has been deleted.";
         } else {
             feedbackToUser = "This recipe does not exist! Please type in a correct recipe name.";
@@ -134,7 +137,7 @@ public class DeleteCommand extends Command {
      */
 
     @Override
-    public CommandResult execute(ArrayList<Ingredient> ingredientList, ArrayList<Recipe> recipeList,
+    public CommandResult    execute(ArrayList<Ingredient> ingredientList, ArrayList<Recipe> recipeList,
                                  ArrayList<Chore> choreList) throws KitchenHelperException {
         return super.execute(ingredientList, recipeList, choreList);
     }
