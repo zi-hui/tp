1dae32449a0da0b24f495db590dff284e90652ac
jinfayap <jinfayap@gmail.com>
2020-03-08 11:16:03 +0800

    Add JUnit Tests

---
+++ src/test/java/seedu/kitchenhelper/command/AddInventoryCommandTest.java
@@ -0,0 +1,39 @@
+package seedu.kitchenhelper.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.kitchenhelper.object.ingredient.Ingredient;
+import seedu.kitchenhelper.object.ingredient.Meat;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AddInventoryCommandTest {
+    
+    @Test
+    void addToCategory_testPass() {
+        ArrayList<Ingredient> ingredientList = new ArrayList<>();
+        new AddInventoryCommand("Beef", "Meat", 30, 20.2, "2020-02-20").addToCategory("Meat", ingredientList);
+        assertTrue(ingredientList.get(0) instanceof Meat);
+        assertEquals("Beef", ingredientList.get(0).getIngredientName());
+        assertEquals("Meat", ingredientList.get(0).getCategoryName());
+        assertEquals(30, ingredientList.get(0).getQuantity());
+        assertEquals(20.2, ingredientList.get(0).getPrice());
+        assertEquals("2020-02-20", ingredientList.get(0).getExpiryDate());
+    }
+    
+    @Test
+    void addToCategory_testFail() {
+        ArrayList<Ingredient> ingredientList = new ArrayList<>();
+        new AddInventoryCommand("Beef", "Meat", 30, 20.2, "2020-02-20").addToCategory("Meat", ingredientList);
+        assertTrue(ingredientList.get(0) instanceof Meat);
+        assertNotEquals("Pork", ingredientList.get(0).getIngredientName());
+        assertNotEquals("Vegetable", ingredientList.get(0).getCategoryName());
+        assertNotEquals(40, ingredientList.get(0).getQuantity());
+        assertNotEquals(50.21, ingredientList.get(0).getPrice());
+        assertNotEquals("2020.02-20", ingredientList.get(0).getExpiryDate());
+    }
+    
+}
