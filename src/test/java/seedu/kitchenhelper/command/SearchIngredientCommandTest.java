8cb2b3db55d6f5b6da87c30ea710eba7f93846ed
jinfayap <jinfayap@gmail.com>
2020-03-22 14:34:08 +0800

    Add Junit Test for SearchIngredientCommand

---
+++ src/test/java/seedu/kitchenhelper/command/SearchIngredientCommandTest.java
@@ -0,0 +1,55 @@
+package seedu.kitchenhelper.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.kitchenhelper.exception.KitchenHelperException;
+import seedu.kitchenhelper.object.Chore;
+import seedu.kitchenhelper.object.Recipe;
+import seedu.kitchenhelper.object.ingredient.Ingredient;
+import seedu.kitchenhelper.object.ingredient.Meat;
+import seedu.kitchenhelper.parser.Parser;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SearchIngredientCommandTest {
+    
+    @Test
+    void execute_notEmpty() throws KitchenHelperException {
+        ArrayList<Ingredient> ingredientArrayList = new ArrayList<>();
+        ArrayList<Recipe> recipeArrayList = new ArrayList<>();
+        ArrayList<Chore> choreArrayList = new ArrayList<>();
+        ingredientArrayList.add(new Meat("Beef", "meat", 3, 20, "2020-03-18"));
+        ingredientArrayList.add(new Meat("Beef cubes", "meat", 3, 20, "2020-03-18"));
+        String expectedOutput = "Here are your matching ingredients in your list\r\n"
+                                + "1.[Meat] Beef Qty:3 $20.0 Exp:2020-03-18\r\n"
+                                + "2.[Meat] Beef cubes Qty:3 $20.0 Exp:2020-03-18";
+        String output = new Parser().parseUserCommand("searchingredient beef")
+                .execute(ingredientArrayList, recipeArrayList, choreArrayList).feedbackToUser;
+        assertEquals(expectedOutput, output);
+    }
+    
+    @Test
+    void execute_empty() throws KitchenHelperException {
+        ArrayList<Ingredient> ingredientArrayList = new ArrayList<>();
+        ArrayList<Recipe> recipeArrayList = new ArrayList<>();
+        ArrayList<Chore> choreArrayList = new ArrayList<>();
+        String expectedOutput = "There are no matching ingredient in your list.";
+        String output = new Parser().parseUserCommand("searchingredient beef")
+                .execute(ingredientArrayList, recipeArrayList, choreArrayList).feedbackToUser;
+        assertEquals(expectedOutput, output);
+    }
+    
+    @Test
+    void execute_notFound() throws KitchenHelperException {
+        ArrayList<Ingredient> ingredientArrayList = new ArrayList<>();
+        ArrayList<Recipe> recipeArrayList = new ArrayList<>();
+        ArrayList<Chore> choreArrayList = new ArrayList<>();
+        ingredientArrayList.add(new Meat("Beef", "meat", 3, 20, "2020-03-18"));
+        ingredientArrayList.add(new Meat("Beef cubes", "meat", 3, 20, "2020-03-18"));
+        String expectedOutput = "There are no matching ingredient in your list.";
+        String output = new Parser().parseUserCommand("searchingredient pork")
+                .execute(ingredientArrayList, recipeArrayList, choreArrayList).feedbackToUser;
+        assertEquals(expectedOutput, output);
+    }
+}
