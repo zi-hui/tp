e3fa79fb9ce05d446720a723413121a09fc7d278
jinfayap <jinfayap@gmail.com>
2020-03-27 19:03:27 +0800

    Change format of the date

--- src/test/java/seedu/kitchenhelper/command/AddIngredientCommandTest.java
+++ src/test/java/seedu/kitchenhelper/command/AddIngredientCommandTest.java
@@ -15,13 +15,13 @@ class AddIngredientCommandTest {
     @Test
     void addToCategory_testPass() {
         ArrayList<Ingredient> ingredientList = new ArrayList<>();
-        new AddIngredientCommand("Beef", "Meat", 30, 20.2, "2020-02-20").addToCategory("Meat", ingredientList);
+        new AddIngredientCommand("Beef", "Meat", 30, 20.2, "20/02/2020").addToCategory("Meat", ingredientList);
         assertTrue(ingredientList.get(0) instanceof Meat);
         assertEquals("Beef", ingredientList.get(0).getIngredientName());
         assertEquals("Meat", ingredientList.get(0).getCategoryName());
         assertEquals(30, ingredientList.get(0).getQuantity());
         assertEquals(20.2, ingredientList.get(0).getPrice());
-        assertEquals("2020-02-20", ingredientList.get(0).getExpiryDate());
+        assertEquals("20/02/2020", ingredientList.get(0).getExpiryDate());
     }
     
     @Test
