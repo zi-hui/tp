a340d76eb4ec2380be9cd3072f56ce29ab963c4e
alwayshuizhen <alwayshuizhen@gmail.com>
2020-03-27 14:06:51 +0800

    added reset command

--- src/main/java/seedu/kitchenhelper/parser/Parser.java
+++ src/main/java/seedu/kitchenhelper/parser/Parser.java
@@ -1,24 +1,8 @@
 package seedu.kitchenhelper.parser;
 
-import seedu.kitchenhelper.command.AddChoreCommand;
-import seedu.kitchenhelper.command.AddRecipeCommand;
-import seedu.kitchenhelper.command.AddIngredientCommand;
-import seedu.kitchenhelper.command.DeleteRecipeCommand;
-import seedu.kitchenhelper.command.DeleteIngredientCommand;
-import seedu.kitchenhelper.command.DeleteChoreCommand;
-import seedu.kitchenhelper.command.ListRecipeCommand;
-import seedu.kitchenhelper.command.ListIngredientCommand;
-import seedu.kitchenhelper.command.ListChoreCommand;
-import seedu.kitchenhelper.command.SearchRecipeCommand;
-import seedu.kitchenhelper.command.SearchIngredientCommand;
-import seedu.kitchenhelper.command.SearchChoreCommand;
-import seedu.kitchenhelper.command.HelpCommand;
-import seedu.kitchenhelper.command.DoneCommand;
-import seedu.kitchenhelper.command.ExitCommand;
-import seedu.kitchenhelper.command.CookRecipeCommand;
-import seedu.kitchenhelper.command.InvalidCommand;
+import seedu.kitchenhelper.command.*;
 import seedu.kitchenhelper.exception.KitchenHelperException;
-import seedu.kitchenhelper.command.Command;
+
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -74,6 +58,8 @@ public class Parser {
             return new SearchRecipeCommand(parameters);
         case SearchChoreCommand.COMMAND_WORD:
             return new SearchChoreCommand(parameters);
+        case ResetCommand.COMMAND_WORD:
+            return new ResetCommand();
         case HelpCommand.COMMAND_WORD:
             return new HelpCommand();
         case ExitCommand.COMMAND_WORD:
