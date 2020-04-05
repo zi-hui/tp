ed3a3ac4cbd966eeba5379a87c7397e1fc8c9bfc
jinfayap <jinfayap@gmail.com>
2020-04-06 00:07:13 +0800

    Add Common Message class

---
+++ src/main/java/seedu/kitchenhelper/common/Messages.java
@@ -0,0 +1,21 @@
+package seedu.kitchenhelper.common;
+
+/**
+ * Container for user visible messages.
+ */
+public class Messages {
+    
+    // SearchCommand Common Message
+    public static final String MESSAGE_SEARCH_EMPTY_LIST = "There are no matching %s in your list.";
+    public static final String MESSAGE_SEARCH_NON_EMPTY_LIST = "Here are your matching %s in your list";
+    public static final String MESSAGE_SEARCH_EMPTY_STRING = "No keyword input detected.\nEnter a keyword.";
+    
+    public static final String EMPTY_STRING = "";
+    public static final String NUMBER_FORMAT = "%d.";
+    public static final String SEARCH_INDEX = " [Location: Index %d]";
+    
+    public static final String MESSAGE_SEARCH_LOG_INFO = "Entering execution of finding matching %s";
+    public static final String MESSAGE_SEARCH_LOG_INFO_EMPTY = "Has non-matching %s";
+    public static final String MESSAGE_SEARCH_LOG_INFO_FOUND = "Found matching %s";
+    
+}
