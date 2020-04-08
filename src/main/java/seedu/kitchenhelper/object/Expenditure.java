379253b00976dae272cf89146b4a4c84e02ecb5d
cheongisabella <cheongisabella@gmail.com>
2020-04-08 19:53:12 +0800

    Add JavaDoc

--- src/main/java/seedu/kitchenhelper/object/Expenditure.java
+++ src/main/java/seedu/kitchenhelper/object/Expenditure.java
@@ -52,6 +52,11 @@ public class Expenditure {
         return onlyInstance;
     }
 
+    /**
+     * Prepare the expenditure information to save in text format.
+     *
+     * @return the text to be saved.
+     */
     public String saveExpenditureFile() {
         String text;
         text = String.format("Expenditure: $%.2f,", totalExpenditure);
@@ -61,6 +66,13 @@ public class Expenditure {
         return text;
     }
 
+    /**
+     * Loads the last saved values of the class variables .
+     *
+     * @param totalExpenditure the last saved total weekly expenditure.
+     * @param amountUsedInCooking the last saved amount used in cooking this week.
+     * @param lastSavedDate the last time user made any changes to expenditure.
+     */
     public void loadExpenditureVariables(double totalExpenditure, double amountUsedInCooking, Date lastSavedDate) {
         this.totalExpenditure = totalExpenditure;
         this.amountUsedInCooking = amountUsedInCooking;
@@ -68,12 +80,18 @@ public class Expenditure {
         renewExpenditureValue();
     }
 
+    /**
+     * Resets the values of all the class variables.
+     */
     public void resetExpenditureData() {
         this.totalExpenditure = 0;
         this.amountUsedInCooking = 0;
         this.lastSavedDate = new Date();
     }
 
+    /**
+     * Renews expenditure values at the start of every week at Monday midnight.
+     */
     public void renewExpenditureValue() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
@@ -95,11 +113,26 @@ public class Expenditure {
         }
     }
 
+    /**
+     * Add to total expenditure every time addingredient command
+     * called to simulate new ingredient purchased.
+     *
+     * @param price Unit price of the ingredient.
+     * @param quantity Quantity of ingredient bought.
+     */
     public void addToExpenditure(double price, int quantity) {
         totalExpenditure += (price * quantity);
         Storage.saveExpenditureData();
     }
 
+    /**
+     * Remove amount from total expenditure when user deletes ingredient
+     * in the case where user deleting because user wrongly added ingredient,
+     * and does not want amount from incorrect addingredient to be included in total expenditure.
+     *
+     * @param ingredientToDelete ingredient to be deleted.
+     * @param quantityToDelete amount of the ingredient to be deleted.
+     */
     public void removeFromExpenditure(Ingredient ingredientToDelete, Integer quantityToDelete) {
         String userResponse = promptUser(PROMPT_REMOVE_FROM_EXPENDITURE);
         if (userResponse.equalsIgnoreCase("yes")) {
@@ -111,6 +144,13 @@ public class Expenditure {
         }
     }
 
+    /**
+     * Adds prices of these ingredients to amount used in cooking,
+     * when user deletes ingredient manually after using to cook.
+     *
+     * @param ingredientToDelete ingredient to be deleted.
+     * @param quantityToDelete amount of the ingredient to be deleted.
+     */
     public void addToAmountUsed(Ingredient ingredientToDelete, Integer quantityToDelete) {
         String userResponse = promptUser(PROMPT_ADD_TO_AMOUNT_USED);
         if (userResponse.equalsIgnoreCase("yes")) {
@@ -120,17 +160,38 @@ public class Expenditure {
         }
     }
 
+    /**
+     * Adds to amount used in cooking whenever user calls cookrecipe command.
+     *
+     * @param ingredientToDelete the ingredient to used in cooking.
+     * @param quantityToDelete the amount of the ingredient used in cooking.
+     */
     public void addAmountForCooking(Ingredient ingredientToDelete, Integer quantityToDelete) {
         amountUsedInCooking += changePrice(ingredientToDelete, quantityToDelete);
         Storage.saveExpenditureData();
     }
 
+    /**
+     * Prompts the user if they want to remove amount from total expenditure or
+     * add amount to amount used in cooking when they call deleteingredient command.
+     *
+     * @param ingredientToDelete ingredient to be deleted.
+     * @param quantityToDelete amount of the ingredient to be deleted.
+     */
     public void editExpenditure(Ingredient ingredientToDelete, Integer quantityToDelete) {
         removeFromExpenditure(ingredientToDelete, quantityToDelete);
         addToAmountUsed(ingredientToDelete, quantityToDelete);
         Storage.saveExpenditureData();
     }
 
+    /**
+     * Calculate the total amount to remove from total expenditure or to add to
+     * amount used in cooking for each ingredient multiplied by their quantity deleted.
+     *
+     * @param ingredientToDelete the ingredient to be deleted.
+     * @param quantityToDelete amount of the ingredient to be deleted.
+     * @return the total change in amount to corresponding class variable.
+     */
     public double changePrice(Ingredient ingredientToDelete, Integer quantityToDelete) {
         double pricePerPiece = ingredientToDelete.getPrice();
         int totalQuantityOfItem = ingredientToDelete.getQuantity();
@@ -141,6 +202,13 @@ public class Expenditure {
         }
     }
 
+    /**
+     * Prompts the user for either a "yes" or "no" response and
+     * assumes a "no" if missing user input.
+     *
+     * @param prompt the question to prompt the user.
+     * @return "yes" or "no".
+     */
     public String promptUser(String prompt) {
         String userResponse;
         System.out.println(prompt);
@@ -156,6 +224,12 @@ public class Expenditure {
         return userResponse;
     }
 
+    /**
+     * Checks if user response is either "yes" or "no".
+     *
+     * @param userResponse the user response.
+     * @return false if user responds with any other input.
+     */
     public boolean isValidResponse(String userResponse) {
         String response = userResponse;
         if (response.equalsIgnoreCase("no") || response.equalsIgnoreCase("yes")) {
@@ -163,7 +237,6 @@ public class Expenditure {
         }
         return false;
     }
-
-
+    
 
 }
